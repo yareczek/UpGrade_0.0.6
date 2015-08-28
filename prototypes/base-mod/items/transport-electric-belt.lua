a56527c1e255d4a082dd0fba7fc0c05b3357fc50
yareczek <vir2000@gmail.com>
2015-08-28 22:39:19 +0200

    Data Lua

---
+++ prototypes/base-mod/items/transport-electric-belt.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+ {
+    type = "item",
+    name = "basic-transport-electric-belt",
+    icon = "__base__/graphics/icons/basic-transport-belt.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "energy-pipe-distribution",
+    order = "a[transport-belt]-a[basic-transport-belt]",
+    place_result = "basic-transport-electric-belt",
+	fuel_value = "4MJ",
+    stack_size = 50
+  }
+ })
